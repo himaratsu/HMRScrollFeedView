0724a0ce9405d2d61020f6490122edb4ab9da344
Ryosuke Hiramatsu <himaratsu@gmail.com>
2014-03-29 17:26:27 +0900

    Initial Commit

---
+++ HMRScrollFeedView/HMRScrollFeedView.h
@@ -0,0 +1,13 @@
+//
+//  HMRScrollFeedView.h
+//  HMRScrollFeedView
+//
+//  Created by himara2 on 2014/03/29.
+//  Copyright (c) 2014年 himara2. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMRScrollFeedView : NSObject
+
+@end
---
+++ HMRScrollFeedView/HMRScrollFeedView.m
@@ -0,0 +1,13 @@
+//
+//  HMRScrollFeedView.m
+//  HMRScrollFeedView
+//
+//  Created by himara2 on 2014/03/29.
+//  Copyright (c) 2014年 himara2. All rights reserved.
+//
+
+#import "HMRScrollFeedView.h"
+
+@implementation HMRScrollFeedView
+
+@end
